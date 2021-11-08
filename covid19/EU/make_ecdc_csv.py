d891aab1b08c494dc548fa1bde35bcea0f4774ab
Dean Karlen <karlen@uvic.ca>
2021-11-08 15:16:24 -0800

    Update EU data (incl new EU hosp data)

--- covid19/EU/make_ecdc_csv.py
+++ covid19/EU/make_ecdc_csv.py
@@ -8,7 +8,6 @@ import datetime
 from datetime import timedelta
 
 t0 = datetime.date(2020,3,1)
-last_date = datetime.date(2020,3,1)
 
 regional_abbreviations = {
     'Austria': 'AT',
@@ -45,33 +44,87 @@ regional_abbreviations = {
     'United Kingdom': 'GB'
 }
 
-hd_by_state = {}
-
-ecdc_file = 'truth_ECDC-Incident Hospitalizations.csv'
+# Starting Nov 8, multiple sources: daily scraped.csv, daily non-eu, weekly for remaining
 data_by_state = {}
+last_date_by_state = {}
 
+raw_files = ['scraped.csv','non-eu.csv']
+
+record_date = None
+for raw_file in raw_files:
+    with open(raw_file) as f:
+        for i,line in enumerate(f):
+            if i > 0:
+                fields = line.split(',')
+                df = fields[2].split('-')
+                record_date = datetime.date(int(df[0]),int(df[1]),int(df[2]))
+                state = fields[1]
+                value = int(fields[3])
+
+                if state not in data_by_state:
+                    data_by_state[state] = {}
+                    data_by_state[state][t0] = 0
+                    last_date_by_state[state] = t0
+                if record_date <= t0:
+                    data_by_state[state][t0] += value
+                else:
+                    data_by_state[state][record_date] = value
+                if record_date > last_date_by_state[state]:
+                    last_date_by_state[state] = record_date
+
+print('Daily hospitalization data provided in scraped.csv and non-eu.csv up to (last date):')
+raw_states = []
+for state in data_by_state:
+    print(state, last_date_by_state[state])
+    raw_states.append(state)
+
+# use weekly data for the rest: split across days of week
+ecdc_file = 'truth_ECDC-Incident Hospitalizations.csv'
+
+date_error = None
 record_date = None
 with open(ecdc_file) as f:
     for i,line in enumerate(f):
         if i > 0:
             fields = line.split(',')
-            df = fields[0].split('-')
+            df = fields[2].split('-')
             record_date = datetime.date(int(df[0]),int(df[1]),int(df[2]))
             state = fields[1]
             value = int(fields[3])
 
-            if state not in data_by_state:
-                data_by_state[state] = {}
-                data_by_state[state][t0] = 0
-            if record_date <= t0:
-                data_by_state[state][t0] += value
-            else:
-                data_by_state[state][record_date] = value
-            if record_date > last_date:
-                last_date = record_date
+            if record_date.weekday() != 5:
+                if date_error is not None:
+                    date_error = state + ' : ' + record_date.isoformat()
 
-print('Hospitalization data provided for:')
-print(','.join(list(data_by_state.keys())))
+            if state not in raw_states:
+
+                if state not in data_by_state:
+                    data_by_state[state] = {}
+                    data_by_state[state][t0] = 0
+                    last_date_by_state[state] = t0
+                if record_date <= t0:
+                    data_by_state[state][t0] += value
+                else:
+                    daily = int(value/7)
+                    extra = value%7
+                    for iday in range(7):
+                        date = record_date - timedelta(days=iday)
+                        data_by_state[state][date] = daily
+                        if iday < extra:
+                            data_by_state[state][date] += 1
+                if record_date > last_date_by_state[state]:
+                    last_date_by_state[state] = record_date
+
+if date_error is not None:
+    print(' *** Error in weekly data:',date_error)
+
+print('Weekly Hospitalization data provided up to (last date):')
+last_date = t0
+for state in data_by_state:
+    if last_date_by_state[state]>last_date:
+        last_date = last_date_by_state[state]
+    if state not in raw_states:
+        print(state, last_date_by_state[state])
 
 with open('eu-ecdc-pypm.csv', 'w') as the_file:
 
